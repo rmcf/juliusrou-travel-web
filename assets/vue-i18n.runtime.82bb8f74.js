e6bf7f93ff0bdc2d8fe297a20f29e25ec738aff6
oleksandr <matsibora.alexander@gmail.com>
2024-10-12 19:13:38 +0300

    origin for main

--- assets/vue-i18n.runtime.82bb8f74.js
+++ assets/vue-i18n.runtime.82bb8f74.js
@@ -1,4 +1,4 @@
-import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,f as Rn,r as Dn,s as kn,j as Mn,T as Fn}from"./index.0aada5b4.js";/*!
+import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,f as Rn,r as Dn,s as kn,j as Mn,T as Fn}from"./index.ecad78c1.js";/*!
   * shared v9.12.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
