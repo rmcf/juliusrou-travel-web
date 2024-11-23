a054ebabb770910b98ccba4ec25251658a32746f
oleksandr <matsibora.alexander@gmail.com>
2024-11-23 16:25:11 +0200

    Bangor

--- assets/vue-i18n.runtime.69e3f569.js
+++ assets/vue-i18n.runtime.69e3f569.js
@@ -1,4 +1,4 @@
-import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,f as Rn,r as Dn,s as kn,j as Mn,T as Fn}from"./index.f5f106f2.js";/*!
+import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,f as Rn,r as Dn,s as kn,j as Mn,T as Fn}from"./index.a507a418.js";/*!
   * shared v9.12.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
