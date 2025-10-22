2205475345da17759a238bb1d22f0d163ca8e99e
Olexander <matsibora.alexander@gmail.com>
2025-10-22 20:53:58 +0300

    fexed shift 2

--- assets/vue-i18n.runtime-BfnVSl5u.js
+++ assets/vue-i18n.runtime-BfnVSl5u.js
@@ -1,4 +1,4 @@
-import{ak as tn,r as nn,al as rn,b as le,w as be,ae as an,am as De,h as st,g as ot,Z as ct,k as ln,o as sn,I as on,$ as cn,an as un}from"./index-kuYVMccY.js";/*!
+import{ak as tn,r as nn,al as rn,b as le,w as be,ae as an,am as De,h as st,g as ot,Z as ct,k as ln,o as sn,I as on,$ as cn,an as un}from"./index-BXraGnvP.js";/*!
   * shared v9.14.2
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
