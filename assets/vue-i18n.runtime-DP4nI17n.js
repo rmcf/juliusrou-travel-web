42fdb6f6b9f5b1596bc0ff226049ee3ed6a0edd4
Olexander <matsibora.alexander@gmail.com>
2025-12-06 16:21:02 +0200

    Munich

--- assets/vue-i18n.runtime-DP4nI17n.js
+++ assets/vue-i18n.runtime-DP4nI17n.js
@@ -1,4 +1,4 @@
-import{ak as tn,r as nn,al as rn,b as le,w as be,ae as an,am as De,h as st,g as ot,Z as ct,k as ln,o as sn,I as on,$ as cn,an as un}from"./index-Dkj2_P1R.js";/*!
+import{ak as tn,r as nn,al as rn,b as le,w as be,ae as an,am as De,h as st,g as ot,Z as ct,k as ln,o as sn,I as on,$ as cn,an as un}from"./index-CccPK7Gh.js";/*!
   * shared v9.14.2
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
