a3e1039d5a8f22b0176c500d980376d584f3ca66
Olexander <matsibora.alexander@gmail.com>
2026-06-21 04:50:25 +0300

    race conditions fix

--- assets/vue-i18n.runtime-BiId3TdJ.js
+++ assets/vue-i18n.runtime-BiId3TdJ.js
@@ -1,4 +1,4 @@
-import{ai as tn,r as nn,aj as rn,a as le,w as be,ac as an,ak as De,h as st,g as ot,X as ct,f as ln,o as sn,F as on,Z as cn,al as un}from"./index-BYgZV76F.js";/*!
+import{ai as tn,r as nn,aj as rn,a as le,w as be,ac as an,ak as De,h as st,g as ot,X as ct,f as ln,o as sn,F as on,Z as cn,al as un}from"./index-Cvl2v1-Q.js";/*!
   * shared v9.14.2
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
